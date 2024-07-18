1ac62fc5f63292c5bdf1e415e3551b9f8b49359d
Scharly <scharly.opitz@gmail.com>
2024-07-18 13:47:56 +0200

    Style top Part

--- src/components/Home/Presentation/TopPart.jsx
+++ src/components/Home/Presentation/TopPart.jsx
@@ -3,9 +3,26 @@ import React from "react";
 export default function TopPart() {
   return (
     <div className="topPart">
-      <h1>
-        La solution pour <br /> votre communication.
-      </h1>
+      <div className="txt">
+        <h1>
+          La solution pour <br /> votre communication.
+        </h1>
+        <p>
+          Transformez vos idées en réalité visuelle avec des solutions de
+          communication personnalisées, rapides et de haute qualité. One comm’
+          vous accompagne dans vos projets.
+        </p>
+      </div>
+      <Buttons />
+    </div>
+  );
+}
+
+function Buttons() {
+  return (
+    <div className="btnContainer">
+      <button>Contactez-nous</button>
+      <button>Un devis ?</button>
     </div>
   );
 }
