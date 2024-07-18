08072d612047260d236ccc800610f466d90b31c7
Scharly <scharly.opitz@gmail.com>
2024-07-18 12:38:24 +0200

    Ajout du composent Presentation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Nav from "../components/Nav/Nav";
+import Presentation from "../components/Home/Presentation";
 
 export default function Home() {
   return (
     <>
       <Nav />
+      <main>
+        <Presentation />
+      </main>
     </>
   );
 }
