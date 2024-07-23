eaa06fd72a379e291a7d643e786c73910fcc0733
Scharly <scharly.opitz@gmail.com>
2024-07-23 13:28:58 +0200

    Ajout section 3 + changement de taille des sections

---
+++ src/components/Home/Section3.jsx
@@ -0,0 +1,15 @@
+export default function Section3() {
+  return (
+    <section className="section3">
+      <h1>
+        Explorez vos différents canaux de <br />
+        communication.
+      </h1>
+      <p>
+        Explorer de nouveaux canaux de communication à l’aide de nos différents
+        services. De la conception graphique à la personnalisation de vos objets
+        publicitaires, nous accompagnons et vous conseillons dans vos projets.
+      </p>
+    </section>
+  );
+}
