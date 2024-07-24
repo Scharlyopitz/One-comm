5d2c1daa6236302b87cfdad2295391c4593ba690
Scharly <scharly.opitz@gmail.com>
2024-07-24 20:13:54 +0200

    Ajout de la section 5

---
+++ src/components/Home/Section5.jsx
@@ -0,0 +1,7 @@
+export default function Section5() {
+  return (
+    <section className="section5">
+      <h1>Ils nous font confiance</h1>
+    </section>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Section1 from "../components/Home/Section1";
 import Section2 from "../components/Home/Section2";
 import Section3 from "../components/Home/Section3";
 import Section4 from "../components/Home/Section4";
+import Section5 from "../components/Home/Section5";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <Section2 />
         <Section3 />
         <Section4 />
+        <Section5 />
       </main>
     </>
   );
