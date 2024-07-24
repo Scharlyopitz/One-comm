43f6d40300a4700a15b3c7ff58e3f02a407e8ec3
Scharly <scharly.opitz@gmail.com>
2024-07-24 20:34:24 +0200

    Style section5

--- src/components/Home/Section5.jsx
+++ src/components/Home/Section5.jsx
@@ -2,6 +2,19 @@ export default function Section5() {
   return (
     <section className="section5">
       <h1>Ils nous font confiance</h1>
+      <Logos />
     </section>
+  );
+}
+
+function Logos() {
+  return (
+    <>
+      <div className="logosContainer">
+        {[...Array(5)].map((_, i) => {
+          return <div key={i} className="logo"></div>;
+        })}
+      </div>
+    </>
   );
 }
