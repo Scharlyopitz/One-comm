05aa7aa5b2e64f6f00b167cc4cbf34ac4151ecdd
Scharly <scharly.opitz@gmail.com>
2024-07-25 12:01:43 +0200

    Ajout page contact dans le router

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer/Footer";
 import Nav from "./components/Nav/Nav";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </>
