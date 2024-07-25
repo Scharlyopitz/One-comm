2cff0e64f32d34432f7cd0cfbb147f51e0cd3120
Scharly <scharly.opitz@gmail.com>
2024-07-25 21:30:48 +0200

    Section 2 Graphicdesign

---
+++ src/components/GraphicDesign/GDSection2.jsx
@@ -0,0 +1,36 @@
+export default function GDSection2() {
+  return (
+    <section className="GDSection2">
+      <Texte
+        h1="Vectorisation de votre logo."
+        p="C’est quoi ? A quoi ça sert ? Explorer de nouveaux canaux de
+        communication à l’aide de nos différents services. De la conception
+        graphique à la personnalisation de vos objets publicitaires, nous
+        accompagnons et vous conseillons dans vos projets."
+      />
+      <Cards />
+    </section>
+  );
+}
+
+function Texte({ h1, p }) {
+  return (
+    <>
+      <h1>{h1}</h1>
+      <p>{p}</p>
+    </>
+  );
+}
+
+function Cards() {
+  return (
+    <div className="cardsContainer">
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+function Card() {
+  return <div className="Card"></div>;
+}
--- src/pages/GraphicDesign.jsx
+++ src/pages/GraphicDesign.jsx
@@ -1,9 +1,11 @@
 import GDSection1 from "../components/GraphicDesign/GDSection1";
+import GDSection2 from "../components/GraphicDesign/GDSection2";
 
 export default function GraphicDesign() {
   return (
     <main>
       <GDSection1 />
+      <GDSection2 />
     </main>
   );
 }
