3e0bf40c6ad4985a627197807d07ffdb282be774
Scharly <scharly.opitz@gmail.com>
2024-07-25 20:39:59 +0200

    Mise au propre du code

--- src/components/Home/Section1.jsx
+++ src/components/Home/Section1.jsx
@@ -3,17 +3,23 @@ import Buttons from "../Buttons";
 export default function Section1() {
   return (
     <section className="section1">
-      <div className="txt">
-        <h1>
-          La solution pour <br /> votre communication.
-        </h1>
-        <p>
-          Transformez vos idées en réalité visuelle avec des solutions de
-          communication personnalisées, rapides et de haute qualité. One comm’
-          vous accompagne dans vos projets.
-        </p>
-      </div>
+      <Texte />
       <Buttons firstBtn="Contactez-nous" secondBtn="Un devis ?" />
     </section>
+  );
+}
+
+function Texte() {
+  return (
+    <div className="txt">
+      <h1>
+        La solution pour <br /> votre communication.
+      </h1>
+      <p>
+        Transformez vos idées en réalité visuelle avec des solutions de
+        communication personnalisées, rapides et de haute qualité. One comm’
+        vous accompagne dans vos projets.
+      </p>
+    </div>
   );
 }
