9111de02e28530451ba99ed83c717f2920f5cc64
Scharly <scharly.opitz@gmail.com>
2024-07-25 13:28:39 +0200

    Ajout section rdvSection

---
+++ src/components/Contact/RdvSection.jsx
@@ -0,0 +1,9 @@
+export default function RdvSection() {
+  return (
+    <section className="rdvSection">
+      <h1>
+        Programmons un rendez-vous <br /> dans nos locaux !
+      </h1>
+    </section>
+  );
+}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,11 @@
 import ContactezNous from "../components/Contact/ContactezNous";
+import RdvSection from "../components/Contact/RdvSection";
 
 export default function Contact() {
   return (
     <main>
       <ContactezNous />
+      <RdvSection />
     </main>
   );
 }
