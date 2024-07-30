92e58466a4c3a9e2ff72b7e409448f0bf3d33741
Scharly <scharly.opitz@gmail.com>
2024-07-30 16:03:48 +0200

    Style images

--- src/components/GraphicDesign/GDSection2.jsx
+++ src/components/GraphicDesign/GDSection2.jsx
@@ -17,6 +17,11 @@ export default function GDSection2() {
         p="Identité visuelle c’est quoi ? a quoi ça sert pour une marque ? ce que ça comprend ? Explorer de nouveaux canaux de communication à l’aide de nos différents services. De la conception graphique à la personnalisation de vos objets publicitaires, nous accompagnons et vous conseillons dans vos projets."
       />
       <Slider />
+      <Texte
+        h1="Création de supports Print & Digital."
+        p="Qu'il s'agisse de brochures, de flyers, de cartes de visite, de kakémono, de mur d’image, de bannières publicitaires, de posts sur les réseaux sociaux ou de sites web, nous concevons des supports de communication qui parlent à votre audience. Nos designs sont non seulement esthétiquement plaisants, mais aussi fonctionnels, garantissant une communication claire et efficace de votre message. Nous nous assurons que chaque création soit adaptée à vos objectifs et à votre audience, tout en respectant les standards les plus élevés du design graphique."
+      />
+      <div className="img">IMAGE</div>
     </section>
   );
 }
@@ -48,7 +53,11 @@ function Slider() {
   return (
     <div className="sliderContainer">
       {[...Array(5)].map((_, i) => {
-        return <div key={i} className="slider"></div>;
+        return (
+          <div key={i} className="slider">
+            IMAGE
+          </div>
+        );
       })}
     </div>
   );
