ac66a45a4941f9c7b18e13eaec02cc577dcc4bc2
Scharly <scharly.opitz@gmail.com>
2024-07-30 17:04:40 +0200

    Création de la nav

--- src/components/ObjetsPublicitaires/OPSection2.jsx
+++ src/components/ObjetsPublicitaires/OPSection2.jsx
@@ -1,3 +1,45 @@
+import { NavLink } from "react-router-dom";
+
 export default function OPSection2() {
-  return <section className="OPSection2">asas</section>;
+  return (
+    <section className="OPSection2">
+      <Nav />
+    </section>
+  );
+}
+
+function Nav() {
+  function Left() {
+    const Links = [
+      { name: "Express 24 / 48h", href: "/" },
+      { name: "Goodies écologiques", href: "/" },
+      { name: "Made in France", href: "/" },
+      { name: "Tous nos goodies", href: "/" },
+    ];
+
+    return (
+      <div className="left">
+        <div className="Menu">Menu</div>
+        {Links.map((link, i) => {
+          return <NavLink to={link.href}>{link.name}</NavLink>;
+        })}
+      </div>
+    );
+  }
+
+  function Right() {
+    return (
+      <div className="right">
+        <input type="text" />
+        <button>Mon devis</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="nav">
+      <Left />
+      <Right />
+    </div>
+  );
 }
