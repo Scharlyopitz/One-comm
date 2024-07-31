372fedab6c5762d3e70b00bbde0d48bb53d39ca6
Scharly <scharly.opitz@gmail.com>
2024-07-31 20:08:35 +0200

    Changement liste component

--- src/components/Liste/Liste.jsx
+++ src/components/Liste/Liste.jsx
@@ -1,14 +1,12 @@
 export default function Liste({ title, date, description }) {
   return (
-    <div className="listesContainer">
-      <div className="liste">
-        <div className="listeImg">IMAGE</div>
-        <div className="listeTxt">
-          <p>{title}</p>
-          <span>{date}</span>
-          <p className="listeDescription">{description}</p>
-          <button>En savoir plus</button>
-        </div>
+    <div className="liste">
+      <div className="listeImg">IMAGE</div>
+      <div className="listeTxt">
+        <p>{title}</p>
+        <span>{date}</span>
+        <p className="listeDescription">{description}</p>
+        <button>En savoir plus</button>
       </div>
     </div>
   );
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -5,11 +5,28 @@ export default function Blog() {
   return (
     <main>
       <MiniSectionTxt title="Explorez notre blog." />
-      <Liste
-        title="Titre de l'article"
-        date="8 juillet"
-        description="Cet article de blog explore l'importance de la communication digitale et du graphisme dans le monde actuel. Il souligne comment une identité visuelle forte et une présence en ligne cohérente peuvent faire la différence pour une entreprise. L'article est structuré pour offrir des conseils pratiques et des exemples concrets de l'impact du graphisme sur la communication digitale."
-      />
+      <div className="listesContainer">
+        <Liste
+          title="Titre de l'article"
+          date="8 juillet"
+          description="Cet article de blog explore l'importance de la communication digitale et du graphisme dans le monde actuel. Il souligne comment une identité visuelle forte et une présence en ligne cohérente peuvent faire la différence pour une entreprise. L'article est structuré pour offrir des conseils pratiques et des exemples concrets de l'impact du graphisme sur la communication digitale."
+        />
+        <Liste
+          title="Titre de l'article"
+          date="8 juillet"
+          description="Cet article de blog explore l'importance de la communication digitale et du graphisme dans le monde actuel. Il souligne comment une identité visuelle forte et une présence en ligne cohérente peuvent faire la différence pour une entreprise. L'article est structuré pour offrir des conseils pratiques et des exemples concrets de l'impact du graphisme sur la communication digitale."
+        />
+        <Liste
+          title="Titre de l'article"
+          date="8 juillet"
+          description="Cet article de blog explore l'importance de la communication digitale et du graphisme dans le monde actuel. Il souligne comment une identité visuelle forte et une présence en ligne cohérente peuvent faire la différence pour une entreprise. L'article est structuré pour offrir des conseils pratiques et des exemples concrets de l'impact du graphisme sur la communication digitale."
+        />
+        <Liste
+          title="Titre de l'article"
+          date="8 juillet"
+          description="Cet article de blog explore l'importance de la communication digitale et du graphisme dans le monde actuel. Il souligne comment une identité visuelle forte et une présence en ligne cohérente peuvent faire la différence pour une entreprise. L'article est structuré pour offrir des conseils pratiques et des exemples concrets de l'impact du graphisme sur la communication digitale."
+        />
+      </div>
     </main>
   );
 }
