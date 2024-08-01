4373225a4810db9869c9493b86734284304dcf43
Scharly <scharly.opitz@gmail.com>
2024-08-01 12:50:00 +0200

    Supression composent TitlePage

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,11 @@
 import ContactezNous from "../components/Contact/ContactezNous";
 import RdvSection from "../components/Contact/RdvSection";
-import TitlePage from "../components/TitlePage/TitlePage";
 
 export default function Contact({ Contacts }) {
   return (
-    <>
-      <TitlePage title="Contact" />
-      <main>
-        <ContactezNous />
-        <RdvSection Contacts={Contacts} />
-      </main>
-    </>
+    <main>
+      <ContactezNous />
+      <RdvSection Contacts={Contacts} />
+    </main>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,15 @@ import Section2 from "../components/Home/Section2";
 import Section3 from "../components/Home/Section3";
 import Section4 from "../components/Home/Section4";
 import Section5 from "../components/Home/Section5";
-import TitlePage from "../components/TitlePage/TitlePage";
 
 export default function Home() {
   return (
-    <>
-      <TitlePage title="Accueil" />
-      <main>
-        <Section1 />
-        <Section2 />
-        <Section3 />
-        <Section4 />
-        <Section5 />
-      </main>
-    </>
+    <main>
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+    </main>
   );
 }
