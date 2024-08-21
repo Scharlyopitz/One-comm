bc075091032d5cda69dcfecd430107e8d0ab0067
Scharly <scharly.opitz@gmail.com>
2024-08-21 12:22:50 +0200

    Changement de style

--- src/pages/Categorie.jsx
+++ src/pages/Categorie.jsx
@@ -10,15 +10,20 @@ export default function Categorie() {
   return (
     <main>
       <TitlePage title={item.title} />
-      <TopPart title={item.title} description={item.description} />
+      <TopPart
+        title={item.title}
+        description={item.description}
+        image={item.image}
+      />
       <Products />
     </main>
   );
 }
 
-function TopPart({ title, description }) {
+function TopPart({ title, description, image }) {
   return (
     <section>
+      <img src={image} alt={title} />
       <h1>{title}</h1>
 
       <p>{description}</p>
