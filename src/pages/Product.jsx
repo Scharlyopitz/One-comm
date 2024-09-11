4f0570a6d364bda576c3a3fbb639f57f87facd60
Scharly <scharly.opitz@gmail.com>
2024-09-11 12:12:33 +0200

    ajout fichier json

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,11 @@
 import { useParams } from "react-router-dom";
 import NavOP from "../components/ObjetsPublicitaires/NavOP";
+import categories from "../assets/categories.json";
 
 export default function Product() {
   const { products } = useParams();
+
+  console.log(categories);
 
   //   const product = categories.find((product) => cate.href === categorie);
 
