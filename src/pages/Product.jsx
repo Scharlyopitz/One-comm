e91b06257ef2000476d2e9b0afd4dcda1da09f86
Scharly <scharly.opitz@gmail.com>
2024-09-11 18:19:22 +0200

    Ajout filtre pour le produit

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,15 +3,24 @@ import NavOP from "../components/ObjetsPublicitaires/NavOP";
 import categories from "../assets/categories.json";
 
 export default function Product() {
-  const { products } = useParams();
+  const { products, categorie } = useParams();
 
-  console.log(categories);
+  const rightCategorie = categories.find((cate) => cate.href === categorie);
 
-  //   const product = categories.find((product) => cate.href === categorie);
+  const rightProduct = rightCategorie.products.find(
+    (c) => c.product.name.split(" ").join("") === products
+  );
+
+  const name = rightProduct.product.name;
+  const price = rightProduct.product.price;
+  const weight = rightProduct.product.weight;
 
   return (
     <main>
       <NavOP />
+      <h1>{name}</h1>
+      <h1>{price}</h1>
+      <h1>{weight}</h1>
     </main>
   );
 }
