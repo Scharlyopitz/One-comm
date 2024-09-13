5084978ad9d8b1199ef8be69c793a4575856cca0
Scharly <scharly.opitz@gmail.com>
2024-09-13 12:27:14 +0200

    Ajout page et projets

--- src/components/Home/Section4.jsx
+++ src/components/Home/Section4.jsx
@@ -1,28 +1,78 @@
+import ImageLine1 from "/Charte-graphique.webp";
+import ImageLine2 from "/Dossier-inscription-événement-padel.webp";
+import ImageLine3 from "/Les-autres-logos.webp";
+import ImageLine4 from "/Nos-couleurs.webp";
+import ImageLine5 from "/Nos-supports.webp";
+import ImageLine6 from "/Tee-shirt-personnalisé.webp";
+
 export default function Section4() {
   return (
     <section className="section4">
       <h1>Nos réalisations</h1>
       <div className="linesContainer">
-        <Line />
-        <Line />
-        <Line />
+        <Line
+          images={[
+            ImageLine1,
+            ImageLine2,
+            ImageLine3,
+            ImageLine4,
+            ImageLine5,
+            ImageLine6,
+          ]}
+        />
+        <Line
+          images={[
+            ImageLine6,
+            ImageLine5,
+            ImageLine4,
+            ImageLine3,
+            ImageLine2,
+            ImageLine1,
+          ]}
+        />
+        <Line
+          images={[
+            ImageLine1,
+            ImageLine2,
+            ImageLine3,
+            ImageLine4,
+            ImageLine5,
+            ImageLine6,
+          ]}
+        />
       </div>
       <Formulaire />
     </section>
   );
 }
 
-function Line() {
+function Line({ images }) {
   return (
     <div className="lineContainer">
       <div className="imgContainer">
-        {[...Array(8)].map((_, i) => {
-          return <div key={i} className="img" />;
+        {images.map((image, i) => {
+          return (
+            <div
+              key={i}
+              className="img"
+              style={{
+                backgroundImage: `URL(${image})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            />
+          );
         })}
       </div>
       <div className="imgContainer">
-        {[...Array(8)].map((_, i) => {
-          return <div key={i} className="img" />;
+        {images.map((image, i) => {
+          return (
+            <div
+              key={i}
+              className="img"
+              style={{ backgroundImage: `URL(${image})` }}
+            />
+          );
         })}
       </div>
     </div>
